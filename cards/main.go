f34442f18c48470e52ec51bff0790bfa56b987ec
hsuyuming <abego452@gmail.com>
2018-05-22 20:41:16 +0800

    type define

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+//go type =>[bool,string,int,float64]
+
+func main() {
+	//var card string = "Ace of Spades"
+	//only use := when we are defined new variable.
+	card := "Ace of Spades"
+
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
 }
