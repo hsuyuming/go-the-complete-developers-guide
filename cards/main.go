461704714390ff2cddf02d3e647eb2f17192d116
hsuyuming <abego452@gmail.com>
2018-05-22 21:25:49 +0800

    array vs slice

--- cards/main.go
+++ cards/main.go
@@ -1,22 +1,31 @@
+/*
+*****************************
+Array v.s Slice (both must be defined with a data type)
+
+Array(primitive data) => always going to be a very fixed length of records
+
+Slice => like an array cna grow or shrink at will.
+
+*****************************
+*/
+
 package main
 
 import (
 	"fmt"
 )
 
-//go type =>[bool,string,int,float64]
-
 func main() {
-	card := newCard()
-	fmt.Println(card)
+	//slice of type string
+	cards := []string{"Ace og Diamonds", newCard()}
+	//append return the new slice not modify original one
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
 	return "Five of Diamonds"
 }
-
-/*
-func newCard() int {
-	return 12
-}
-*/
