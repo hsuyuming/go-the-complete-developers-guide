68be2fc1c7ae6e15262b4d04204072cd579e3079
hsuyuming <abego452@gmail.com>
2018-05-22 21:04:14 +0800

    Lesson3. quit3-5 same package how they interact each other

--- helloworld/main.go
+++ helloworld/main.go
@@ -3,15 +3,6 @@
 //Reusable : like code dependencies or library.(helper function or some code will used in future).
 package main
 
-//package apple
-
-import "fmt"
-
 func main() {
-	fmt.Println(getTitle())
-}
-
-//go don't need to define function first(from quit)
-func getTitle() string {
-	return "Harry Potter"
+	printState()
 }
