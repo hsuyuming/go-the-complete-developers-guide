9b81f1d59f50b7d85d59434cbd7001e2961d62ce
hsuyuming <abego452@gmail.com>
2018-05-22 20:10:02 +0800

    create app

---
+++ helloworld/main.go
@@ -0,0 +1,12 @@
+//package=project=workspace
+//package have two types (Executable and Reusable)
+//Reusable : like code dependencies or library.(helper function or some code will used in future).
+package main
+
+//package apple
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi there")
+}
