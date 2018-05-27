c6b2e9be02ec41f075673434fdb787370244640e
hsuyuming <abego452@gmail.com>
2018-05-27 13:09:10 +0800

    learn receiver function

--- cards/main.go
+++ cards/main.go
@@ -11,19 +11,13 @@ Slice => like an array cna grow or shrink at will.
 
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	//slice of type string
-	cards := []string{"Ace og Diamonds", newCard()}
+	cards := deck{"Ace og Diamonds", newCard()}
 	//append return the new slice not modify original one
 	cards = append(cards, "Six of Spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
