0c9ccc3f5bf6a5c7b67142a414d5ef7e9eccbab7
hsuyuming <abego452@gmail.com>
2018-05-30 22:19:38 +0800

    define a new struct

--- structs/main.go
+++ structs/main.go
@@ -1,10 +1,21 @@
 package main
 
+import (
+	"fmt"
+)
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
-
+	//first way to create new person
+	alex := person{"Alex", "Anderson"}
+	fmt.Println(alex)
+	//second way to define a new struct
+	alex = person{firstName: "Alex", lastName: "Anderson"}
+	fmt.Println(alex)
+	fmt.Println(alex.firstName)
+	fmt.Println(alex.lastName)
 }
