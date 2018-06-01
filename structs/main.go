7cd7bfca21d86b144f00407d1dcb35268c44f395
hsuyuming <abego452@gmail.com>
2018-06-01 23:10:05 +0800

    pointer:go is pass by value language

--- structs/main.go
+++ structs/main.go
@@ -17,13 +17,22 @@ type person struct {
 }
 
 func (p person) print() {
+	fmt.Printf("%p\n", &p) //result =>0xc42001e140
 	fmt.Printf("%+v \n", p)
 }
 
 func (p person) updateName(newFirstName string) {
 	p.firstName = newFirstName
+	fmt.Printf("%p\n", &p) //result => 0xc42001e180
+	p.print()              //result=> {firstName:jimmy lastName:Party contactInfo:{email:jim@gmail.com zipCode:9400}}
 }
 
+/*
+go is what we refer to as a pass by value language
+whenevet we pass some value into a function
+go will take that value(date) and copy all of that data inside that struct
+and then place it inside of new some new container(memory address)
+*/
 func main() {
 	//go => when ever we are declaring multi line structures
 	//every single line must have a comma,even if it is the last property declaration or the last
@@ -35,6 +44,7 @@ func main() {
 			zipCode: 9400,
 		},
 	}
+	fmt.Printf("%p\n", &jim) //result =>0xc42001e100
 	jim.updateName("jimmy")
-	jim.print()
+	//jim.print() //result=>{firstName:Jim lastName:Party contactInfo:{email:jim@gmail.com zipCode:9400}}
 }
