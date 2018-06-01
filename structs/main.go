ed7c1f9cc6d72a4f43860ed4095b6ae7add83992
hsuyuming <abego452@gmail.com>
2018-06-01 22:46:37 +0800

    Refactor:only use contactInfo

--- structs/main.go
+++ structs/main.go
@@ -4,16 +4,28 @@ import (
 	"fmt"
 )
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	//contact   contactInfo
+	contactInfo
 }
 
 func main() {
-	//third way to define new struct
-	var alex person
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	//go => when ever we are declaring multi line structures
+	//every single line must have a comma,even if it is the last property declaration or the last
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 9400,
+		},
+	}
+	fmt.Printf("%+v \n", jim)
 }
